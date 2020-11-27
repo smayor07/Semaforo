8922af18484659e8ed8b009507a8e5e10ee5c957
SebaMayor <smayor.ti2017@outlook.com>
2020-11-26 22:58:19 -0300

    Commit inicial projeto Semaforo.

---
+++ ProdutorConsumidorSemaforo/Projeto Principal/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Projeto_Principal
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("=========== PRODUTOR/CONSUMIDOR - SEMÁFORO ===========");
+            Console.WriteLine("======================================================");
+            Console.WriteLine("========== EQUIPE: Amanda Lapa e Sebá Mayor ==========");
+            Console.WriteLine("======================================================");
+            Console.WriteLine("");
+
+            SemaforoClass SEM = new SemaforoClass();
+            Console.Write("Número de valores a serem produzidos:");
+            int N = int.Parse(Console.ReadLine());
+            Console.WriteLine("");
+
+            for (int i = 0; i < N; i++)
+            {
+                SEM.INICIA();
+            }
+
+            Console.WriteLine("SOMATÓRIO FINAL:" + SEM.GetSoma());
+            Console.WriteLine("=======================================================");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ProdutorConsumidorSemaforo/Projeto Principal/SemaforoClass.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Projeto_Principal
+{
+    class SemaforoClass
+    {
+        private Semaphore SEM;
+        private int BUFFER = 0;
+        private int SOMA = 0;
+
+        public void INICIA()
+        {
+            SEM = new Semaphore(1, 2);
+            Thread T1 = new Thread(Produtor);
+            Thread T2 = new Thread(Consumidor);
+
+            T1.Start();
+            T2.Start();
+
+            Thread.Sleep(1000);
+            SEM.Release();
+        }
+
+        public void Produtor()
+        {
+            BUFFER++;
+            Console.WriteLine("Produzindo valor............");
+            Console.WriteLine("Produzido - VALOR: " + BUFFER);
+            Console.WriteLine("----------------------------");
+            SEM.WaitOne();
+        }
+
+        public void Consumidor()
+        {
+            SOMA += BUFFER;
+            Console.WriteLine("Consumindo valor............");
+            Console.WriteLine("Consumido - VALOR: " + BUFFER);
+            Console.WriteLine("----------------------------");
+            SEM.WaitOne();
+        }
+
+        public int GetSoma()
+        {
+            return SOMA;
+        }
+
+    }
+}
